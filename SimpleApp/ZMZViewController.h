ccc5c2cd6112eab2b3a1a4d293d98452b260d997
maggiezheng <295004646@qq.com>
2020-02-20 11:36:53 +0800

    首次上传，还有问题

---
+++ SimpleApp/ZMZViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZMZViewController.h
+//  SimpleApp
+//
+//  Created by 郑梦真 on 2020/2/18.
+//  Copyright © 2020 MaggieZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMZViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SimpleApp/ZMZViewController.m
@@ -0,0 +1,32 @@
+//
+//  ZMZViewController.m
+//  SimpleApp
+//
+//  Created by 郑梦真 on 2020/2/18.
+//  Copyright © 2020 MaggieZ. All rights reserved.
+//
+
+#import "ZMZViewController.h"
+
+@interface ZMZViewController ()
+
+@end
+
+@implementation ZMZViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
